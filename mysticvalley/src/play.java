357885efa8b0422b58023885984305ec368c4d5b
GiovanaSimoes <75585216+GiovanaSimoes@users.noreply.github.com>
2021-05-27 15:58:54 -0300

    Update play.java

--- mysticvalley/src/play.java
+++ mysticvalley/src/play.java
@@ -35,10 +35,10 @@ public class play {
 
         do {
             System.out.println("1- Jogar ");
-            System.out.println("2- InstruÁıes ");
-            System.out.println("3- CrÈditos ");
+            System.out.println("2- Instru√ß√µes ");
+            System.out.println("3- Cr√©ditos ");
             System.out.println("4- Sair ");
-            System.out.print("Escolha uma opÁ„o: ");
+            System.out.print("Escolha uma op√ß√£o: ");
             escolha = entrada.nextInt();
 
             switch (escolha) {
@@ -46,59 +46,59 @@ public class play {
                 case 1:
                     break;
                 case 2:
-                    System.out.println("VocÍ escolheu {INSTRU«’ES}");
+                    System.out.println("Voc√™ escolheu {INSTRU√á√ïES}");
                     System.out.printf("\n");
                     System.out.println("Em uma ilha muito distante, existe uma vila chamada Mystic Valley.");
-                    System.out.println("Em uma noite de comemoraÁıes acontece um roubo onde o item mais raro de toda ilha È levado,");
-                    System.out.println("ent„o um detetive com seus poderes sobrenaturais se dispıe a encontrar o culpado.");
-                    System.out.println("Nessa aventura vocÍs desvendar„o esse mistÈrio que intriga os moradores de Mystic Valley.");
+                    System.out.println("Em uma noite de comemora√ß√µes acontece um roubo onde o item mais raro de toda ilha √© levado,");
+                    System.out.println("ent√£o um detetive com seus poderes sobrenaturais se disp√µe a encontrar o culpado.");
+                    System.out.println("Nessa aventura voc√™s desvendar√£o esse mist√©rio que intriga os moradores de Mystic Valley.");
                     System.out.printf("\n");
-                    System.out.println("O jogo Mystic Valley È um jogo de multipla escolha, em que suas escolhas influenciam diretamente o jogo, ");
+                    System.out.println("O jogo Mystic Valley √© um jogo de multipla escolha, em que suas escolhas influenciam diretamente o jogo, ");
 
-                    System.out.println("podendo mudar os caminhos e a histÛria, o intuito dessas escolhas s„o praticar a sua lÛgica, esperamos que vocÍ se divirta e que aumente a sua lÛgica !");
+                    System.out.println("podendo mudar os caminhos e a hist√≥ria, o intuito dessas escolhas s√£o praticar a sua l√≥gica, esperamos que voc√™ se divirta e que aumente a sua l√≥gica !");
                     break;
                 case 3:
-                    System.out.println("VocÍ escolheu {CR…DITOS}");
+                    System.out.println("Voc√™ escolheu {CR√âDITOS}");
                     System.out.println("Leonardo Messias");
                     System.out.println("Thalles Negreiro");
                     System.out.println("Laleska Fernandes");
-                    System.out.println("Giovana Simıes");
+                    System.out.println("Giovana Sim√µes");
                     System.out.println("Guilherme Profirio");// nome de cada integrante
                     break;
                 case 4:
-                    System.err.println(" VocÍ saiu !");
+                    System.err.println(" Voc√™ saiu !");
                     break;
                 default:
-                    System.out.println("OpÁ„o inv·lida !!!");
+                    System.out.println("Op√ß√£o inv√°lida !!!");
                     break;
 
             }
         } while (!(escolha == 4 || escolha == 1));
 
         if (escolha == 1) {
-            System.out.println("VocÍ escolheu {JOGAR}");
-            //continuar o cÛdigo aqui 
+            System.out.println("Voc√™ escolheu {JOGAR}");
+            //continuar o c√≥digo aqui 
             System.out.printf("\n");
-            System.out.println("CapÌtulo  I - Potion of immortality\n"
-                    + "No meio de um oceano t„o t„o distante existe uma ilha misteriosa, quase t„o grande quanto um paÌs, ");
+            System.out.println("Cap√≠tulo  I - Potion of immortality\n"
+                    + "No meio de um oceano t√£o t√£o distante existe uma ilha misteriosa, quase t√£o grande quanto um pa√≠s, ");
             System.out.println("nessa ilha existe vilarejo chamado Mystic Valley ");
-            System.out.println("onde habita diversas criaturas m·gicas de todas as espÈcies,");
-            System.out.println("que s„o separadas por cl„s. O cl„ dos bruxos È um dos mais poderosos de toda ilha,");
-            System.out.println("pois eles possuem conhecimentos antigos, fÛrmulas e poÁıes m·gicas,");
-            System.out.println("uma dessas poÁıes talvez a mais rara e cobiÁada de toda ilha, È a potion of immortality,");
-            System.out.println("onde o mito diz: Aquele que a consumir nunca morrer·.");
+            System.out.println("onde habita diversas criaturas m√°gicas de todas as esp√©cies,");
+            System.out.println("que s√£o separadas por cl√£s. O cl√£ dos bruxos √© um dos mais poderosos de toda ilha,");
+            System.out.println("pois eles possuem conhecimentos antigos, f√≥rmulas e po√ß√µes m√°gicas,");
+            System.out.println("uma dessas po√ß√µes talvez a mais rara e cobi√ßada de toda ilha, √© a potion of immortality,");
+            System.out.println("onde o mito diz: Aquele que a consumir nunca morrer√°.");
             System.out.printf("\n");
-            System.out.println("Todos os forasteiros de todos os cantos do mundo s„o atraÌdos por essa ideia,");
-            System.out.println("com a cobiÁa de a possui-la para si prÛprio, ");
-            System.out.println("mas ao chegarem nas margens da ilha apenas aqueles seres m·gicos ou aqueles que provarem seu valor poderam adentrar-la,");
-            System.out.println("atravÈs de um enigma implantado por m·gia......");
+            System.out.println("Todos os forasteiros de todos os cantos do mundo s√£o atra√≠dos por essa ideia,");
+            System.out.println("com a cobi√ßa de a possui-la para si pr√≥prio, ");
+            System.out.println("mas ao chegarem nas margens da ilha apenas aqueles seres m√°gicos ou aqueles que provarem seu valor poderam adentrar-la,");
+            System.out.println("atrav√©s de um enigma implantado por m√°gia......");
             System.out.printf("\n");
 
             do {
-                System.out.println("Em um jardim: A) todas as flores, menos duas s„o rosas ");
-                System.out.println("B) todas as flores, menos duas s„o margaridas");
-                System.out.println("C) todas as flores, menos duas s„o orquÌdeas.");
-                System.out.println("Quantas flores h· no jardim?");// desafio 1
+                System.out.println("Em um jardim: A) todas as flores, menos duas s√£o rosas ");
+                System.out.println("B) todas as flores, menos duas s√£o margaridas");
+                System.out.println("C) todas as flores, menos duas s√£o orqu√≠deas.");
+                System.out.println("Quantas flores h√° no jardim?");// desafio 1
 
                 System.out.println("a) " + urna.get(0));
                 System.out.println("b) " + urna.get(1));
@@ -146,7 +146,7 @@ public class play {
                 }
 
             } while ((!(resposta.equalsIgnoreCase(alternativa1))));
-            System.out.println("ParabÈns !!!! VocÍ entrou na ilha, aqui comeÁa a sua histÛria.....");
+            System.out.println("Parab√©ns !!!! Voc√™ entrou na ilha, aqui come√ßa a sua hist√≥ria.....");
             System.out.println("BOA SORTE");// final capitulo 1
 
         }
@@ -156,33 +156,33 @@ public class play {
         static void capitulo2() throws Exception{
         Scanner entrada = new Scanner(System.in);
         String escolhacaminho,caminhoalternativo = "Tentar resolver sozinho.", 
-                caminhocerto = "Pedir ajuda a um jovem bruxo que est· a observar.",
+                caminhocerto = "Pedir ajuda a um jovem bruxo que est√° a observar.",
                 escolhacap2,enigcap2 = "10";
         
-        System.out.println("CapÌtulo II  - O roubo\n\n");
-        Digita("Em uma noite de comemoraÁıes onde todos os cl„s estavam reunidos no grande sal„o, o alarme do cl„ dos bruxos "
+        System.out.println("Cap√≠tulo II  - O roubo\n\n");
+        Digita("Em uma noite de comemora√ß√µes onde todos os cl√£s estavam reunidos no grande sal√£o, o alarme do cl√£ dos bruxos "
                 + "\necoa por todos os cantos da cidade, mostrando assim que algo muito ruim havia acontecido. Rapidamente todos "
                 + "daquela festa se apavoram e procuram pelo xerife da cidade que foi incumbido a preservar a ordem de toda vila. "
-                + "\nO xerife diz ñ ìRe˙na todos meus homens em frente ‡ delegacia imediatamente!î",TimeUnit.MILLISECONDS, temp_dialog);
+                + "\nO xerife diz ‚Äì ‚ÄúRe√∫na todos meus homens em frente √† delegacia imediatamente!‚Äù",TimeUnit.MILLISECONDS, temp_dialog);
         Digita("\n\nCom todos os homens do xerife reunidos ele faz um pronunciamento sobre o ocorrido. "
-                + "\nO xerife diz ñ ìVamos nos separar e descobrir o que foi levado dos bruxos, a partir de agora ninguÈm entra "
-                + "ou sai desta ilhaî."
-                + "\nAo chegarem na regi„o dos bruxos, logo descobrem que o item levado foi o mais valioso de toda ilha, o templo"
+                + "\nO xerife diz ‚Äì ‚ÄúVamos nos separar e descobrir o que foi levado dos bruxos, a partir de agora ningu√©m entra "
+                + "ou sai desta ilha‚Äù."
+                + "\nAo chegarem na regi√£o dos bruxos, logo descobrem que o item levado foi o mais valioso de toda ilha, o templo"
                 + "\nonde estava guardada por muitos anos havia sido violado por um usurpador, mas para qualquer ser que desejasse "
-                + "\nadentra-la, precisaria primeiro decifrar o enigma escrito em lÌngua de bruxo logo acima do port„o de entrada."
+                + "\nadentra-la, precisaria primeiro decifrar o enigma escrito em l√≠ngua de bruxo logo acima do port√£o de entrada."
                 ,TimeUnit.MILLISECONDS, temp_dialog);
 
         do{
             
-                 List <String> list = Arrays.asList("Pedir ajuda a um jovem bruxo que est· a observar."
-                ,"Tentar resolver sozinho.","Invadir com forÁa bruta, mas sabendo que h· um feitiÁo de proteÁ„o.");
+                 List <String> list = Arrays.asList("Pedir ajuda a um jovem bruxo que est√° a observar."
+                ,"Tentar resolver sozinho.","Invadir com for√ßa bruta, mas sabendo que h√° um feiti√ßo de prote√ß√£o.");
                 Collections.shuffle(list);
                 
-                Digita("\nVocÍ tem algumas opÁıes para resoluÁ„o deste enigma:"
-                        + "\n1 ñ " + list.get(0)
-                        + "\n2 ñ " + list.get(1)
-                        + "\n3 ñ " + list.get(2)
-                        + "\nIndique qual caminho deseja seguir em n˙mero: ",TimeUnit.MILLISECONDS, temp_dialog);
+                Digita("\nVoc√™ tem algumas op√ß√µes para resolu√ß√£o deste enigma:"
+                        + "\n1 ‚Äì " + list.get(0)
+                        + "\n2 ‚Äì " + list.get(1)
+                        + "\n3 ‚Äì " + list.get(2)
+                        + "\nIndique qual caminho deseja seguir em n√∫mero: ",TimeUnit.MILLISECONDS, temp_dialog);
                 escolhacaminho = entrada.next();
                 
                 switch(escolhacaminho){
@@ -191,10 +191,10 @@ public class play {
                             escolhacaminho = caminhocerto;
                             
                         }else if(list.get(0).equals(caminhoalternativo)){
-                            Digita("\n(n„o consigo traduzir sozinho, pois est· em uma lÌngua desconhecida), pensou o xerife"
+                            Digita("\n(n√£o consigo traduzir sozinho, pois est√° em uma l√≠ngua desconhecida), pensou o xerife"
                                     + "\ntente novamente!\n",TimeUnit.MILLISECONDS, temp_dialog);
                         }else{
-                            Digita("\nvocÍ foi empurrado para tr·s e todos riram"
+                            Digita("\nvoc√™ foi empurrado para tr√°s e todos riram"
                                     + "\ntente novamente!\n",TimeUnit.MILLISECONDS, temp_dialog);
                         }
                         break;
@@ -203,10 +203,10 @@ public class play {
                             escolhacaminho = caminhocerto;
                             
                         }else if(list.get(1).equals(caminhoalternativo)){
-                            Digita("\n(n„o consigo traduzir sozinho, pois est· em uma lÌngua desconhecida), pensou o xerife"
+                            Digita("\n(n√£o consigo traduzir sozinho, pois est√° em uma l√≠ngua desconhecida), pensou o xerife"
                                     + "\ntente novamente!\n",TimeUnit.MILLISECONDS, temp_dialog);
                         }else{
-                            Digita("\nvocÍ foi empurrado para tr·s e todos riram"
+                            Digita("\nvoc√™ foi empurrado para tr√°s e todos riram"
                                     + "\ntente novamente!\n",TimeUnit.MILLISECONDS, temp_dialog);
                         }
                         break;
@@ -214,36 +214,36 @@ public class play {
                         if(list.get(2).equals(caminhocerto)){
                             escolhacaminho = caminhocerto;
                         }else if(list.get(2).equals(caminhoalternativo)){
-                            Digita("\n(n„o consigo traduzir sozinho, pois est· em uma lÌngua desconhecida), pensou o xerife"
+                            Digita("\n(n√£o consigo traduzir sozinho, pois est√° em uma l√≠ngua desconhecida), pensou o xerife"
                                     + "\ntente novamente!\n",TimeUnit.MILLISECONDS, temp_dialog);
                         }else{
-                            Digita("\nvocÍ foi empurrado para tr·s e todos riram"
+                            Digita("\nvoc√™ foi empurrado para tr√°s e todos riram"
                                     + "\ntente novamente!\n",TimeUnit.MILLISECONDS, temp_dialog);
                         }
                         break;
                     default:
-                            System.out.println("OpÁ„o invalida!"
+                            System.out.println("Op√ß√£o invalida!"
                                     + "\nTente novamente.");
                 }
         }while ((!(escolhacaminho.equals(caminhocerto))));
         
             Digita("\nCom a ajuda do jovem bruxo a placa foi traduzida, "
-                    + "\npara que enfim o xerife consiga prosseguir com a sua investigaÁ„o" 
+                    + "\npara que enfim o xerife consiga prosseguir com a sua investiga√ß√£o" 
                     + "\nrevelando assim um novo desafio que diz:",TimeUnit.MILLISECONDS, temp_dialog);
-            Digita("\nNo caminho de casa atÈ o templo, um mago conta 10 ·rvores a sua direita."
-                    + "\nApÛs ida ao templo, ele retorna para casa e conta 10 ·rvores a sua esquerda.",TimeUnit.MILLISECONDS, temp_dialog);
+            Digita("\nNo caminho de casa at√© o templo, um mago conta 10 √°rvores a sua direita."
+                    + "\nAp√≥s ida ao templo, ele retorna para casa e conta 10 √°rvores a sua esquerda.",TimeUnit.MILLISECONDS, temp_dialog);
             
             do{
                 List<String> list = Arrays.asList("20","15","10","30","25");
                 Collections.shuffle(list);
                 
-                Digita("\nQuantas ·rvores ele viu no total nesse dia?"
+                Digita("\nQuantas √°rvores ele viu no total nesse dia?"
                         + "\nA) " + list.get(0)
                         + "\nB) " + list.get(1)
                         + "\nC) " + list.get(2)
                         + "\nD) " + list.get(3)
                         + "\nE) " + list.get(4)
-                        + "\nQual È a Alternativa Correta:",TimeUnit.MILLISECONDS, temp_dialog);
+                        + "\nQual √© a Alternativa Correta:",TimeUnit.MILLISECONDS, temp_dialog);
                 escolhacap2 = entrada.next();
                 
                 switch (escolhacap2.toUpperCase()) {
@@ -291,7 +291,7 @@ public class play {
                             else if(!(escolhacap2.equals(enigcap2))){
                                 System.out.println("\nAlternativa Incorreta!");
                             }else{
-                              System.out.println("OpÁ„o invalida!");  
+                              System.out.println("Op√ß√£o invalida!");  
                             }
                             
                         
@@ -299,14 +299,13 @@ public class play {
                             
             }
         }while((!(escolhacap2.equalsIgnoreCase(enigcap2))));
-            Digita("\n\nAlternativa correta! VocÍ conseguiu desfazer o feitiÁo de proteÁ„o!"
-                    + "\n\nPois, s„o as mesmas 10 ·rvores vistas de diferente perspectivas."
-                    + "\nNa ida, ·rvores estavam · direita do mago, mas na volta"
-                    + "\nquando ele estava no sentido contr·rio, as ·rvores podiam ser vista · esquerda\n",TimeUnit.MILLISECONDS, temp_dialog);
+            Digita("\n\nAlternativa correta! Voc√™ conseguiu desfazer o feiti√ßo de prote√ß√£o!"
+                    + "\n\nPois, s√£o as mesmas 10 √°rvores vistas de diferente perspectivas."
+                    + "\nNa ida, √°rvores estavam √° direita do mago, mas na volta"
+                    + "\nquando ele estava no sentido contr√°rio, as √°rvores podiam ser vista √° esquerda\n",TimeUnit.MILLISECONDS, temp_dialog);
             
             /*FIM CAP ||*/
             }
 }
-
-
+           Digita("Teste");
 
